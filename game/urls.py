25ccca786045d707db3d2b443002e0a37e261f5a
Mykola <62808554+nikola876@users.noreply.github.com>
2025-05-26 22:30:43 +0300

    Update urls.py

--- game/urls.py
+++ game/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .views import RegisterView  # добавили
+from .views import RegisterView 
 
 urlpatterns = [
     path('', views.game_view, name='home'),
-    path('register/', RegisterView.as_view(), name='register'),  # добавили
+    path('register/', RegisterView.as_view(), name='register'),
 ]
